afc3c71a05c891549dc004de895f3e36a7328d76
Himanshi Sharma <himanshish456@gmail.com>
2024-08-09 16:45:51 +0530

    redis configuration

---
+++ src/main/java/com/example/springAndRedis/SpringAndRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springAndRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springAndRedis/SpringAndRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springAndRedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
